c6f7f1ef4515e7488350b756affec97fca852d41
Hafijul Islam <hafijul233@gmail.com>
2021-11-21 03:54:14 +0600

    resource update for inline icon

--- resources/views/bootstrap4/inline/email.blade.php
+++ resources/views/bootstrap4/inline/email.blade.php
@@ -7,9 +7,31 @@
         $msg = $errors->first($name) ?? null;
 
         if(isset($required) && $required == true)
-            $options['required'] = 'required';
+            $options['required'] = 'required'
     @endphp
-    {!! Form::email($name, $default, array_merge($options, $attributes)) !!}
+    <div class="input-group">
+        @if(isset($position) && $position == 'before')
+            <div class="input-group-prepend">
+                <div class="input-group-text">
+                    @if(!empty($icon))
+                        <span class="{{ $icon }}"></span>
+                    @endif
+                </div>
+            </div>
+        @endif
+
+        {!! Form::email($name, $default, array_merge($options, $attributes)) !!}
+
+        @if(isset($position) && $position == 'after')
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    @if(!empty($icon))
+                        <span class="{{ $icon }}"></span>
+                    @endif
+                </div>
+            </div>
+        @endif
+    </div>
 
     {!! Form::nError($name, $msg) !!}
 </div>

7971ee613c313967ab8355bfaa4293a07fc9eab1
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-08-11 01:47:03 +0600

    doc added

--- src/Facades/Form.php
+++ src/Facades/Form.php
@@ -5,6 +5,7 @@ namespace Hafijul233\Form\Facades;
 use Closure;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
+use phpDocumentor\Reflection\Types\Boolean;
 
 /**
  * Class Form
@@ -16,7 +17,11 @@ use Illuminate\Support\Facades\Facade;
  * @method static \Illuminate\Support\HtmlString getIdAttribute(string $name, array $attributes)
  * @method static \Illuminate\Support\HtmlString getValueAttribute(string $name, string $value = null)
  * @method static \Illuminate\Support\HtmlString token()
- * @method static \Illuminate\Support\HtmlString void assertSentTo(mixed $notifiable, string|Closure $notification, callable $callback = null)
+ * @method static \Illuminate\Support\HtmlString close()
+ * @method static \Illuminate\Support\HtmlString label(string $name, string $value = null, array $options = [], bool $escape_html = true)
+ * @method static \Illuminate\Support\HtmlString text(string $name, string $value = null, array $options = [])
+ * @method static \Illuminate\Support\HtmlString password(string $name, string $value = null, array $options = [])
+ *
  * @method static \Illuminate\Support\HtmlString void assertSentOnDemandTimes(string $notification, int $times = 1)
  * @method static \Illuminate\Support\HtmlString void assertSentToTimes(mixed $notifiable, string $notification, int $times = 1)
  * @method static \Illuminate\Support\HtmlString void assertTimesSent(int $expectedCount, string $notification)

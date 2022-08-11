492f8c2c8f43ac9a9360efff57e8b5ed77856813
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-08-12 02:27:19 +0600

    doc added

--- src/Facades/Form.php
+++ src/Facades/Form.php
@@ -2,33 +2,39 @@
 
 namespace Hafijul233\Form\Facades;
 
-use Closure;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
-use phpDocumentor\Reflection\Types\Boolean;
+use Illuminate\Support\HtmlString;
 
 /**
  * Class Form
  *
- * @method static \Illuminate\Support\HtmlString model(mixed $model, array  $options = [])
- * @method static \Illuminate\Support\HtmlString open(array $options = [])
- * @method static \Illuminate\Support\HtmlString hidden(string $name, mixed $value = null, array $options = [])
- * @method static \Illuminate\Support\HtmlString input(string $type, string $name, mixed $value = null, array $options = [])
- * @method static \Illuminate\Support\HtmlString getIdAttribute(string $name, array $attributes)
- * @method static \Illuminate\Support\HtmlString getValueAttribute(string $name, string $value = null)
- * @method static \Illuminate\Support\HtmlString token()
- * @method static \Illuminate\Support\HtmlString close()
- * @method static \Illuminate\Support\HtmlString label(string $name, string $value = null, array $options = [], bool $escape_html = true)
- * @method static \Illuminate\Support\HtmlString text(string $name, string $value = null, array $options = [])
- * @method static \Illuminate\Support\HtmlString password(string $name, array $options = [])
- * @method static \Illuminate\Support\HtmlString range(string $name, string $value = null, array $options = [])
- * @method static \Illuminate\Support\HtmlString range(string $name, string $value = null, array $options = [])
+ * @method static HtmlString model(mixed $model, array $options = [])
+ * @method static HtmlString open(array $options = [])
+ * @method static HtmlString hidden(string $name, mixed $value = null, array $options = [])
+ * @method static HtmlString input(string $type, string $name, mixed $value = null, array $options = [])
+ * @method static HtmlString getIdAttribute(string $name, array $attributes)
+ * @method static HtmlString getValueAttribute(string $name, string $value = null)
+ * @method static HtmlString token()
+ * @method static HtmlString close()
+ * @method static HtmlString label(string $name, string $value = null, array $options = [], bool $escape_html = true)
+ * @method static HtmlString text(string $name, string $value = null, array $options = [])
+ * @method static HtmlString password(string $name, array $options = [])
+ * @method static HtmlString range(string $name, string $value = null, array $options = [])
+ * @method static HtmlString search(string $name, string $value = null, array $options = [])
+ * @method static HtmlString email(string $name, string $value = null, array $options = [])
+ * @method static HtmlString tel(string $name, string $value = null, array $options = [])
+ * @method static HtmlString url(string $name, string $value = null, array $options = [])
+ * @method static HtmlString number(string $name, string $value = null, array $options = [])
+ * @method static HtmlString textarea(string $name, string $value = null, array $options = [])
+ * @method static HtmlString file(string $name, array $options = [])
+ * @method static HtmlString date(string $name, string $value = null, array $options = [])
+ * @method static HtmlString datetime(string $name, string $value = null, array $options = [])
+ * @method static HtmlString datetimeLocal(string $name, string $value = null, array $options = [])
+ * @method static HtmlString time(string $name, string $value = null, array $options = [])
+ * @method static HtmlString week(string $name, string $value = null, array $options = [])
+ * @method static HtmlString selectRange(string $name, string $begin, string $end, string $selected = null, array $options = [])
+ * @method static HtmlString select(string $name, array $list = [], string $selected = null, array $selectAttributes = [], array $optionsAttributes = [], array $optgroupsAttributes = [])
  *
- * @method static \Illuminate\Support\HtmlString void assertSentOnDemandTimes(string $notification, int $times = 1)
- * @method static \Illuminate\Support\HtmlString void assertSentToTimes(mixed $notifiable, string $notification, int $times = 1)
- * @method static \Illuminate\Support\HtmlString void assertTimesSent(int $expectedCount, string $notification)
- * @method static \Illuminate\Support\HtmlString void send(Collection|array|mixed $notifiables, $notification)
- * @method static \Illuminate\Support\HtmlString void sendNow(Collection|array|mixed $notifiables, $notification)
  *
  * @see \Hafijul233\Form\Builders\FormBuilder
  * @package Hafijul233\Form\Facades
